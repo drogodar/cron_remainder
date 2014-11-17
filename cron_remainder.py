d910c164ca3153c82d58442e581bd87933839208
drozdek <samret13@gmail.com>
2014-11-17 11:58:38 +0800

    Add functionality for week days. Rebuilt next_day_calc(). Fixed some bugs.

--- cron_remainder.py
+++ cron_remainder.py
@@ -3,7 +3,7 @@
 import re
 import subprocess
 from datetime import datetime, timedelta
-from monthdelta import MonthDelta
+from monthdelta import monthdelta
 import calendar
 
 today = datetime.today()
@@ -22,10 +22,10 @@ def crontab_processing():
     if out:
         crontab_rows = out.split("\n")
 
-    # Compile RegEx for beauty
+    # Compile RegEx for beauty TODO: Non-Standard Characters - '?', 'L', 'M', '#'
     p = re.compile('''
     ^                # beginning of the line
-    (\d|\*|,|-|/)*   # any number of digits, asterisks, commas, '-' and '/'  TODO: ?, 'L', 'M'
+    (\d|\*|,|-|/)*   # any number of digits, asterisk, commas, '-' and '/'
     $                # end of the line
     ''', re.VERBOSE)
 
@@ -35,134 +35,140 @@ def crontab_processing():
     # Split rows for five time elements and job.
     for cron_row in crontab_rows:
         row_elements = [k for k in cron_row.split(' ', 5)]
-        time_list = [row_elements[i] for i in range(len(row_elements) - 1)]
+        time_list = [element for element in [row_elements[i] for i in range(len(row_elements) - 1)]
+                     if re.search(p, element) and len(row_elements) == 6]
 
         # Filter rows through the RegEx and check for len.
         # After that we have only "right" rows with jobs
-        if [element for element in time_list if re.search(p, element) and len(row_elements) == 6]:
+        if len(time_list) == 5:
             job = row_elements[5][:-1]
+            next_date = today
+            true_weekday = False
+            cnt = 0
 
-            # Try to find previous job's executions.
-            try:
-                sys_log_file = open('/var/log/cron', 'r')  # Specify here cron log destination for your distr.
-                                                           # Not required for current version.
-                log_lines = [k for k in sys_log_file.readlines()]
+            while true_weekday is False:
+                # Try to find previous job's executions.
+                try:
+                    sys_log_file = open('/var/log/cron', 'r')  # Specify here cron log destination for your distr.
+                                                               # Not required for current version.
+                    log_lines = [k for k in sys_log_file.readlines()]
 
-                # Find and parse the last execution time
-                for log_line in log_lines:
-                    if log_line.find(job) > -1:
-                        last_exec_date = log_line[:12]
+                    # Find and parse the last execution time.
+                    for log_line in log_lines:
+                        if log_line.find(job) > -1:
+                            last_exec_date = log_line[:12]
 
-                last_minute = last_exec_date.split('  ', )[1].split(' ')[1].split(':')[1]
-                last_hour = last_exec_date.split('  ', )[1].split(' ')[1].split(':')[0]
-                last_day = last_exec_date.split('  ', )[1].split(' ')[0]
-                last_month = month_names[last_exec_date.split('  ')[0]]
+                    last_minute = last_exec_date.split('  ', )[1].split(' ')[1].split(':')[1]
+                    last_hour = last_exec_date.split('  ', )[1].split(' ')[1].split(':')[0]
+                    last_day = last_exec_date.split('  ', )[1].split(' ')[0]
+                    last_month = month_names[last_exec_date.split('  ')[0]]
 
-                # Execute func to calculate the next execution time
-                next_date = next_date_calc(time_list, [last_minute, last_hour, last_day, last_month])
-            except Exception:
-                sys_log_file = []
-                next_date = next_date_calc(time_list)
-            finally:
-                if sys_log_file:
-                    sys_log_file.close()
+                    # Execute func to calculate the next execution time.
+                    next_date = next_date_calc(time_list, next_date, [last_minute, last_hour, last_day, last_month])
+                except Exception:
+                    sys_log_file = []
+                    next_date = next_date_calc(time_list, next_date)
+                finally:
+                    if sys_log_file:
+                        sys_log_file.close()
 
-            # Echo the result
+                # Check for day of week.
+                if next_date.weekday() in parse_list(time_list[4], 'weekday'):
+                    true_weekday = True
+                else:
+                    next_date = next_date_calc(time_list, next_date + timedelta(days=1))
+
+                cnt += 1
+                if cnt == 5:
+                    true_weekday = True
+
+            # Print the result
             print "\"" + job + "\"" + " job will be executed " + str(next_date)
 
 
-def next_date_calc(cron_date, last_date=[]):
+def next_date_calc(pattern, start_date, last_date=[]):
     """Calculate the date when job will be executed."""
 
-    next_date = today  # Initial date
+    next_date = start_date  # Initial date
     not_now = False
 
-    # Calculate date values one by one. In decreasing order. If '*' - stay the same and skip the block.
-    if cron_date[3] != '*':  # Month
-        next_month = []
-        month_list = parse_list(cron_date[3], 'month')
-        approp_list = filter(lambda x: (x >= today.month), sorted(month_list))
+    # Calculate date values one by one. In decreasing order.
 
-        for month in approp_list:
-            next_month = month
-            break
+    next_month = []
+    month_list = parse_list(pattern[3], 'month')
+    approp_list = filter(lambda x: (x >= start_date.month), sorted(month_list))
 
-        # If next month is within this year, then just add it.
-        if next_month:
-            next_date += MonthDelta((next_month - today.month))
-            if next_month != today.month:
-                not_now = True
-        # If not, then get the lowest number from month_list and go with it to next year
-        else:
-            next_date += MonthDelta((12 - today.month + month_list[0]))
+    for month in approp_list:
+        next_month = month
+        break
+
+    # If next month is within this year, then just add it.
+    if next_month:
+        next_date += monthdelta((next_month - start_date.month))
+        if next_month != start_date.month:
             not_now = True
-
-    if cron_date[2] != '*':  # Day
-        next_day = []
-        day_list = parse_list(cron_date[2], 'day')
-        approp_list = filter(lambda x: (x >= today.day), sorted(day_list))
-
-        for day in approp_list:
-            next_day = day
-            break
-
-        # If not this month, then just get the lowest number from day_list with other the same values.
-        if not_now:
-            if day_list[0] < today.day:
-                next_date = datetime(next_date.year, next_date.month, day_list[0],
-                                     next_date.hour, next_date.minute, 0)
-        elif next_day:
-            next_date += timedelta(next_day - today.day)
-            if next_day != today.day:
-                not_now = True
-        else:
-            next_date += timedelta(calendar.monthrange(today.year, today.month)[1] - today.day + day_list[0])
-            not_now = True
-
-    if cron_date[1] != '*':  # Hour
-        next_hour = []
-        hour_list = parse_list(cron_date[1], 'hour')
-        approp_list = filter(lambda x: (x >= today.hour), sorted(hour_list))
-
-        for hour in approp_list:
-            next_hour = hour
-            break
-
-        if not_now:
-            if hour_list[0] < today.hour:
-                next_date = datetime(next_date.year, next_date.month, next_date.day,
-                                     hour_list[0], next_date.minute, 0)
-        elif next_hour:
-            next_date += timedelta(hours=next_hour - today.hour)
-            if next_hour != today.hour:
-                not_now = True
-        else:
-            next_date += timedelta(hours=24 - today.hour + hour_list[0])
-            not_now = True
-
-    if cron_date[0] != '*':  # Minute
-        next_min = []
-        min_list = parse_list(cron_date[0], 'minute')
-        approp_list = filter(lambda x: (x >= today.minute), min_list)
-
-        for min in approp_list:
-            next_min = min
-            break
-
-        if not_now:
-            next_date = datetime(next_date.year, next_date.month, next_date.day,
-                                 next_date.hour, min_list[0], 0)
-
-        elif next_min:
-            next_date += timedelta(minutes=next_min - today.minute)
-        else:
-            next_date += timedelta(minutes=60 - today.minute + min_list[0])
+    # If not, then get the lowest number from month_list and go with it to next year
     else:
-        if not_now:
+        next_date += monthdelta((12 - start_date.month + month_list[0]))
+        not_now = True
+
+    next_day = []
+    day_list = parse_list(pattern[2], 'day')
+    approp_list = filter(lambda x: (x >= start_date.day), sorted(day_list))
+
+    for day in approp_list:
+        next_day = day
+        break
+
+    # If not this month, then just get the lowest number from day_list with other the same values.
+    if not_now:
+        if day_list[0] < start_date.day:
+            next_date = datetime(next_date.year, next_date.month, day_list[0],
+                                 next_date.hour, next_date.minute, 0)
+    elif next_day:
+        next_date += timedelta(next_day - start_date.day)
+        if next_day != start_date.day:
+            not_now = True
+    else:
+        next_date += timedelta(calendar.monthrange(start_date.year, start_date.month)[1] - start_date.day + day_list[0])
+        not_now = True
+
+    next_hour = []
+    hour_list = parse_list(pattern[1], 'hour')
+    approp_list = filter(lambda x: (x >= start_date.hour), sorted(hour_list))
+
+    for hour in approp_list:
+        next_hour = hour
+        break
+
+    if not_now:
+        if hour_list[0] < start_date.hour:
             next_date = datetime(next_date.year, next_date.month, next_date.day,
-                                 next_date.hour, 0, 0)
-        else:
-            next_date += timedelta(minutes=1)
+                                 hour_list[0], next_date.minute, 0)
+    elif next_hour:
+        next_date += timedelta(hours=next_hour - start_date.hour)
+        if next_hour != start_date.hour:
+            not_now = True
+    else:
+        next_date += timedelta(hours=24 - start_date.hour + hour_list[0])
+        not_now = True
+
+    next_min = []
+    min_list = parse_list(pattern[0], 'minute')
+    approp_list = filter(lambda x: (x >= start_date.minute), min_list)
+
+    for min in approp_list:
+        next_min = min
+        break
+
+    if not_now:
+        next_date = datetime(next_date.year, next_date.month, next_date.day,
+                             next_date.hour, min_list[0], 0)
+
+    elif next_min:
+        next_date += timedelta(minutes=next_min - start_date.minute)
+    else:
+        next_date += timedelta(minutes=60 - start_date.minute + min_list[0])
 
     return next_date
 
@@ -183,8 +189,14 @@ def parse_list(values, vtype):
     elif vtype == 'minute':
         first_relement = 0
         second_relement = 59
+    elif vtype == 'weekday':
+        first_relement = 0
+        second_relement = 6
     else:
         return 'Wrong type designation.'
+
+    if values == '*':
+        return range(first_relement, second_relement+1)
 
     # If '*/5'(for example), then return a list with 'every fifth'.
     if values.find('/') > -1 and values.find('*') > -1:
@@ -202,7 +214,12 @@ def parse_list(values, vtype):
             else:
                 value_list.append(int(value))
 
-    return value_list
+    # In cron '0' and '7' is for Sunday, but in Python there is no '7' for Sunday, so replace it to '0'.
+    if vtype == 'weekday' and value_list.count(7) > 0:
+        value_list.remove(7)
+        value_list.insert(0, 0)
+
+    return list(set(value_list))
 
 
 if __name__ == '__main__':
