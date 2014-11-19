348f4e3475fa421c3152947e2d7492585790eaf0
drozdek <samret13@gmail.com>
2014-11-19 15:17:10 +0800

    Smooth and optimize code. Heal some bugs. Remove unnecessary syslog-thing.

--- README.md
+++ README.md
@@ -3,23 +3,35 @@ cron_remainder by drozdek
 
 <p>This application can tell you when next job execution will be for your current user.</p>
 
-<p>It processes through your crontab file and syslogs. It doesn't require root privileges.</p>
+<p>It processes through your crontab file. It doesn't require root privileges.</p>
 
-<p>I made the alias in the .bashrc for this and launch along with bash or manually.
-You can do the same or use it in any other way.</p>
+<h5>Supported features:</h5>
+<ul>
+<li>'*' - full date range;</li>
+<li>'X-Y' - from the X to the Y;</li>
+<li>'X,Y' - the X and the Y;</li>
+<li>Combination of previous two;</li>
+<li>'X/Y' - every Y through the X (can be *)</li>
+<li>'Mon-Wed','Jun,Jul,Sep-Dec' - along with numbers for weekdays and months;</li>
+</ul>
 
-<p>Among other things, you can use any part of this code. I don't mind.</p>
+<h5>The way to use:</h5>
+<p>You can just launch the script OR made an alias in the .bashrc for this and launch it manually or along with starting bash.</p>
 
-<h4>CHANGES:</h4>
-<ul><li><b>17 Nov 2014:</b> Add functionality for week days. Rebuilt next_day_calc(). Fixed some bugs.</li>
+
+<h6>CHANGES:</h6>
+<ul>
+<li><b>19 Nov 2014:</b> Smooth and optimize code. Heal some bugs. Remove unnecessary syslog-thing.</li>
+<li><b>17 Nov 2014:</b> Add functionality for week days. Rebuilt next_day_calc(). Fixed some bugs.</li>
 <li><b>09 Nov 2014:</b> Expand the functionality. Now you can specify '*/5', '2-10/2' or even 
 '1,3,10-23,25,29-30' views for month, day, hour and minute. Also fixed some bugs.
-Still don't have compatibility with '?', 'L', 'M' and days of week. Coming soon.</li>
+Still don't have compatibility with days of week. Coming soon.</li>
 <li><b>08 Nov 2014:</b> The base of future application. Still some bugs and defects, but work.
 For now, compatible only with set of rules like this -
 "0 12 * 1,3,7,10,12 * echo 'Donate blood.'". (digits, *, ',', any job)
 Also you need specify cron logs destination.
-For CentOS it will be '/var/log/cron'.</li></ul>
+For CentOS it will be '/var/log/cron'.</li>
+</ul>
 
 
 
--- cron_remainder.py
+++ cron_remainder.py
@@ -1,4 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
+
+# cron_remainder processes user's crontab file and prints the next execution time of jobs
+# Copyright (C) 2014  Semen Pichugin
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# My email: samret13@gmail.com
 
 import re
 import subprocess
@@ -8,6 +26,10 @@ import calendar
 
 today = datetime.today()
 today = datetime(today.year, today.month, today.day, today.hour, today.minute)
+weekday_names = {"MON": '1', "TUE": '2', "WED": '3', "THU": '4', "FRI": '5', "SAT": '6', "SUN": '7'}
+month_names = {"JAN": '1', "FEB": '2', "MAR": '3', "APR": '4', "MAY": '5', "JUN": '6', "JUL": '7', "AUG": '8',
+                   "SEP": '9', "OCT": '10', "NOV": '11', "DEC": '12'}
+regex_pattern = "^(\d|\*|,|-|/|" + "|".join(weekday_names.keys()) + "|" + "|".join(month_names.keys()) + ")*$"
 
 
 def crontab_processing():
@@ -21,72 +43,51 @@ def crontab_processing():
         return err  # possible error: 'no crontab for drozdek'
     if out:
         crontab_rows = out.split("\n")
+    else:
+        return 'Crontab file has no lines.'
 
-    # Compile RegEx for beauty TODO: Non-Standard Characters - '?', 'L', 'M', '#'
-    p = re.compile('''
-    ^                # beginning of the line
-    (\d|\*|,|-|/)*   # any number of digits, asterisk, commas, '-' and '/'
-    $                # end of the line
-    ''', re.VERBOSE)
+    # Compile RegEx for beauty
+    p = re.compile(regex_pattern, re.IGNORECASE)
+    job_found = False
 
-    month_names = {"Jan": '1', "Feb": '2', "Mar": '3', "Apr": '4', "May": '5', "Jun": '6', "Jul": '7', "Aug": '8',
-                   "Sep": '9', "Oct": '10', "Nov": '11', "Dec": '12'}
+    print 'Starting to search for appropriate lines...'
 
     # Split rows for five time elements and job.
     for cron_row in crontab_rows:
         row_elements = [k for k in cron_row.split(' ', 5)]
+
+        # Filter rows through the RegEx
         time_list = [element for element in [row_elements[i] for i in range(len(row_elements) - 1)]
                      if re.search(p, element) and len(row_elements) == 6]
 
-        # Filter rows through the RegEx and check for len.
-        # After that we have only "right" rows with jobs
+        # Check for a len and then we have only right rows with jobs
         if len(time_list) == 5:
             job = row_elements[5][:-1]
             next_date = today
             true_weekday = False
-            cnt = 0
+            job_found = True
 
+            # Calc next date
+            next_date = next_date_calc(time_list, next_date)
+
+            # Check for day of week.
             while true_weekday is False:
-                # Try to find previous job's executions.
-                try:
-                    sys_log_file = open('/var/log/cron', 'r')  # Specify here cron log destination for your distr.
-                                                               # Not required for current version.
-                    log_lines = [k for k in sys_log_file.readlines()]
-
-                    # Find and parse the last execution time.
-                    for log_line in log_lines:
-                        if log_line.find(job) > -1:
-                            last_exec_date = log_line[:12]
-
-                    last_minute = last_exec_date.split('  ', )[1].split(' ')[1].split(':')[1]
-                    last_hour = last_exec_date.split('  ', )[1].split(' ')[1].split(':')[0]
-                    last_day = last_exec_date.split('  ', )[1].split(' ')[0]
-                    last_month = month_names[last_exec_date.split('  ')[0]]
-
-                    # Execute func to calculate the next execution time.
-                    next_date = next_date_calc(time_list, next_date, [last_minute, last_hour, last_day, last_month])
-                except Exception:
-                    sys_log_file = []
-                    next_date = next_date_calc(time_list, next_date)
-                finally:
-                    if sys_log_file:
-                        sys_log_file.close()
-
-                # Check for day of week.
-                if next_date.weekday() in parse_list(time_list[4], 'weekday'):
+                if next_date.isoweekday() in parse_list(time_list[4], 'weekday'):
                     true_weekday = True
                 else:
-                    next_date = next_date_calc(time_list, next_date + timedelta(days=1))
-
-                cnt += 1
-                if cnt == 5:
-                    true_weekday = True
+                    next_date = datetime(next_date.year, next_date.month, next_date.day, 0, 0, 0) + timedelta(days=1)
+                    next_date = next_date_calc(time_list, next_date)
 
             # Print the result
             print "\"" + job + "\"" + " job will be executed " + str(next_date)
 
+    if job_found is False:
+        return 'No appropriate lines have been found.'
+    else:
+        return 'Success!'
 
-def next_date_calc(pattern, start_date, last_date=[]):
+
+def next_date_calc(pattern, start_date):
     """Calculate the date when job will be executed."""
 
     next_date = start_date  # Initial date
@@ -94,16 +95,16 @@ def next_date_calc(pattern, start_date, last_date=[]):
 
     # Calculate date values one by one. In decreasing order.
 
+    # Month
     next_month = []
     month_list = parse_list(pattern[3], 'month')
     approp_list = filter(lambda x: (x >= start_date.month), sorted(month_list))
 
-    for month in approp_list:
-        next_month = month
-        break
+    if approp_list != []:
+        next_month = approp_list[0]
 
     # If next month is within this year, then just add it.
-    if next_month:
+    if next_month != []:
         next_date += monthdelta((next_month - start_date.month))
         if next_month != start_date.month:
             not_now = True
@@ -112,20 +113,20 @@ def next_date_calc(pattern, start_date, last_date=[]):
         next_date += monthdelta((12 - start_date.month + month_list[0]))
         not_now = True
 
+    # Day
     next_day = []
-    day_list = parse_list(pattern[2], 'day')
+    day_list = parse_list(pattern[2], 'day', [next_date.year, next_date.month])
     approp_list = filter(lambda x: (x >= start_date.day), sorted(day_list))
 
-    for day in approp_list:
-        next_day = day
-        break
+    if approp_list != []:
+        next_day = approp_list[0]
 
     # If not this month, then just get the lowest number from day_list with other the same values.
     if not_now:
         if day_list[0] < start_date.day:
             next_date = datetime(next_date.year, next_date.month, day_list[0],
                                  next_date.hour, next_date.minute, 0)
-    elif next_day:
+    elif next_day != []:
         next_date += timedelta(next_day - start_date.day)
         if next_day != start_date.day:
             not_now = True
@@ -133,19 +134,19 @@ def next_date_calc(pattern, start_date, last_date=[]):
         next_date += timedelta(calendar.monthrange(start_date.year, start_date.month)[1] - start_date.day + day_list[0])
         not_now = True
 
+    # Hour
     next_hour = []
     hour_list = parse_list(pattern[1], 'hour')
     approp_list = filter(lambda x: (x >= start_date.hour), sorted(hour_list))
 
-    for hour in approp_list:
-        next_hour = hour
-        break
+    if approp_list != []:
+        next_hour = approp_list[0]
 
     if not_now:
         if hour_list[0] < start_date.hour:
             next_date = datetime(next_date.year, next_date.month, next_date.day,
                                  hour_list[0], next_date.minute, 0)
-    elif next_hour:
+    elif next_hour != []:
         next_date += timedelta(hours=next_hour - start_date.hour)
         if next_hour != start_date.hour:
             not_now = True
@@ -153,19 +154,19 @@ def next_date_calc(pattern, start_date, last_date=[]):
         next_date += timedelta(hours=24 - start_date.hour + hour_list[0])
         not_now = True
 
+    # Minute
     next_min = []
     min_list = parse_list(pattern[0], 'minute')
     approp_list = filter(lambda x: (x >= start_date.minute), min_list)
 
-    for min in approp_list:
-        next_min = min
-        break
+    if approp_list != []:
+        next_min = approp_list[0]
 
     if not_now:
         next_date = datetime(next_date.year, next_date.month, next_date.day,
                              next_date.hour, min_list[0], 0)
 
-    elif next_min:
+    elif next_min != []:
         next_date += timedelta(minutes=next_min - start_date.minute)
     else:
         next_date += timedelta(minutes=60 - start_date.minute + min_list[0])
@@ -173,16 +174,24 @@ def next_date_calc(pattern, start_date, last_date=[]):
     return next_date
 
 
-def parse_list(values, vtype):
+def parse_list(entry_values, vtype, props=[]):
     """Parse the input params to the list of values using cron rules."""
 
+    values = entry_values.upper()
     value_list = []
+
     if vtype == 'month':
+        for month, num in month_names.items():
+            values = values.replace(month, str(num))
         first_relement = 1
         second_relement = 12
     elif vtype == 'day':
-        first_relement = 1
-        second_relement = 30
+        try:
+            first_relement = 1
+            second_relement = calendar.monthrange(props[0], props[1])[1]
+        except IndexError:
+            print 'Wrong props in the parse_list() for ' + vtype
+            exit()
     elif vtype == 'hour':
         first_relement = 0
         second_relement = 23
@@ -190,8 +199,10 @@ def parse_list(values, vtype):
         first_relement = 0
         second_relement = 59
     elif vtype == 'weekday':
-        first_relement = 0
-        second_relement = 6
+        for weekday, num in weekday_names.items():
+            values = values.replace(weekday, str(num))
+        first_relement = 1
+        second_relement = 7
     else:
         return 'Wrong type designation.'
 
@@ -201,10 +212,12 @@ def parse_list(values, vtype):
     # If '*/5'(for example), then return a list with 'every fifth'.
     if values.find('/') > -1 and values.find('*') > -1:
         value_list = range(first_relement, second_relement + 1, int(values.split('/')[1]))
+
     # If '3-9/3'(for example), then return a list with [3,6,9].
     elif values.find('/') > -1:
         value = values.split('/')[0]
         value_list = range(int(value.split('-')[0]), int(value.split('-')[1])+1, int(values.split('/')[1]))
+
     # If '1,2-4,6,9,10-12', then return a list with [1, 2, 3, 4, 6, 9, 10, 11, 12].
     else:
         for value in values.split(','):
@@ -215,12 +228,12 @@ def parse_list(values, vtype):
                 value_list.append(int(value))
 
     # In cron '0' and '7' is for Sunday, but in Python there is no '7' for Sunday, so replace it to '0'.
-    if vtype == 'weekday' and value_list.count(7) > 0:
-        value_list.remove(7)
-        value_list.insert(0, 0)
+    if vtype == 'weekday' and value_list.count(0) > 0:
+        value_list.remove(0)
+        value_list.append(7)
 
     return list(set(value_list))
 
 
 if __name__ == '__main__':
-    crontab_processing()
+    print crontab_processing()
